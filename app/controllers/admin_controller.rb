0c3ec8c7481687c8fa922854a3a547e9a5d6e1f5
Bimofr <smarie-c@e2r1p4.42.fr>
2014-11-24 15:09:14 +0100

    method veille dans admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,11 +5,44 @@ class AdminController < ApplicationController
   end
 
   def update_veille
+  	begin
+	  	veille = Veille.find(param[:id])
+	rescue Exception => e
+		redirect_to user_path, :flash => { :alert => "la veille cherché n'a pas été trouvée." } and return
+	end
+	veille.titre = params[:titre]
+	veille.texte = params[:texte]
+	begin
+	  	veille.save
+	rescue Exception => e
+		redirect_to user_path, :flash => { :alert => "la veille n'a pas été mise à jour." } and return
+	end
+	redirect_to user_path, :flash => { :notice => "la veille a été mise à jour." } and return
   end
 
   def delete_veille
+  	begin
+	  	veille = Veille.find(param[:id])
+	rescue Exception => e
+		redirect_to user_path, :flash => { :alert => "la veille cherché n'a pas été trouvée." } and return
+	end
+	begin
+	  	veille.destroy
+	rescue Exception => e
+		redirect_to user_path, :flash => { :alert => "la veille n'a pas été supprimée." } and return
+	end
+	redirect_to user_path, :flash => { :notice => "la veille a été supprimée." } and return
   end
 
   def create_veille
+  	veille = Veille.new
+  	veille.titre = params[:titre]
+	veille.texte = params[:texte]
+  	begin
+	  	veille.save
+	rescue Exception => e
+		redirect_to user_path, :flash => { :alert => "la veille n'a pas été créé." } and return
+	end
+	redirect_to user_path, :flash => { :notice => "la veille a été créé." } and return
   end
 end
